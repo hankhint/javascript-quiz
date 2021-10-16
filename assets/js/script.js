e4850cc7edd3b8dbdf1678276e304132fb3199f8
HankHint <hankhint@gmail.com>
2021-10-16 00:00:01 -0400

    html skeleton and css somewhat done

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+var questions = [
+    {
+    prompt: "Javascript first appeared in 1995",
+    answer: "True"
+}
+{
+    prompt: "Event bubbling and event capturing are the same",
+    answer: "False"
+
+    /*With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
+
+With capturing, the event is first captured by the outermost element and propagated to the inner elements.*/
+}
+{
+    prompt: "Event delegation uses event bubbling",
+    answer: "True"
+}
+{
+    prompt: "the \<link\> tag is used to link the HTML to the javascript file",
+    answer: "False"
+    /* <script> tag is used */
+}
+{
+    prompt: "let, const, and var can all be used to define variables",
+    answer: "True"
+}
+{
+    prompt: "there will be no errors if you redefine a constant",
+    answer: "False"
+    /* a const variable can't be redefined */
+}
+{
+    prompt: "There are six data types in JS",
+    answer: "False"
+    /* there are five: string, number, object, undefined, Boolean */
+}
+{
+    prompt: "three equal signs is the strict equality operator",
+    answer: "True"
+}
+{
+    prompt: "backslash in the escape character in JS",
+    answer: "True"
+}
+]
+
+var score = 0;
+
+for (var i=0; i < questions.length; i++ ) {
+    
+}
+
+FormData.addEventListener('submit', e => {
+    e.preventDefault()
+})
