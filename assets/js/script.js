8b221a85990a6a13a60899f17a680ff396d85629
HankHint <hankhint@gmail.com>
2021-10-17 01:25:12 -0400

    countdown timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,115 @@
-
-
 var score = 0;
 
 const startBtn = document.getElementById("startBtn");
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('prompt');
-const btnGridElement = document.getElementById('btnGrid')
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("prompt");
+const btnGridElement = document.getElementById("btnGrid");
 
-// these two variables need to be redefined later, so use let: 
+// these two variables need to be redefined later, so use let:
 let shuffledQuestions, currentQuestionIndex;
 
-startBtn.addEventListener('click', startGame);
+//timer
+
+var timerEl = document.getElementById('timer');
+var mainEl = document.getElementById('timer-container');
+
+function countdown() {
+    var timeLeft = 85;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = 'Time is up.';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+      }
+    }, 1000);
+  }
+  
+
+startBtn.addEventListener("click", () => {
+  countdown();
+  startGame();
+});
 
 function startGame() {
-    console.log("here")
-  startBtn.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5);
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  startBtn.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
-function showQuestion(question) {
-questionElement.innerText = question.prompt;
 
+function showQuestion(question) {
+  questionElement.innerText = question.prompt;
+  
 }
 function selectAnswer() {}
 
-function resetState {
-    
-}
+
+
+
+
 
 const questions = [
-    {
-      prompt: "Javascript first appeared in 1995",
-      answer: "True",
-    },
-    {
-      prompt: "Event bubbling and event capturing are the same",
-      answer: "False",
-  
-      /*With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
+  {
+    prompt: "Javascript first appeared in 1995",
+    answer: "correct",
+  },
+  {
+    prompt: "Event bubbling and event capturing are the same",
+    answer: "incorrect",
+
+    /*With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
   
        With capturing, the event is first captured by the outermost element and propagated to the inner elements.*/
-    },
-    {
-      prompt: "Event delegation uses event bubbling",
-      answer: "True",
-    },
-    {
-      prompt: "the <link> tag is used to link the HTML to the javascript file",
-      answer: "False",
-      /* <script> tag is used */
-    },
-    {
-      prompt: "let, const, and var can all be used to define variables",
-      answer: "True",
-    },
-    {
-      prompt: "there will be no errors if you redefine a constant",
-      answer: "False",
-      /* a const variable can't be redefined */
-    },
-    {
-      prompt: "There are six data types in JS",
-      answer: "False",
-      /* there are five: string, number, object, undefined, Boolean */
-    },
-    {
-      prompt: "three equal signs is the strict equality operator",
-      answer: "True",
-    },
-    {
-      prompt: "backslash in the escape character in JS",
-      answer: "True",
-    },
-  ];
+  },
+  {
+    prompt: "Event delegation uses event bubbling",
+    answer: "correct",
+  },
+  {
+    prompt: "the <link> tag is used to link the HTML to the javascript file",
+    answer: "incorrect",
+    /* <script> tag is used */
+  },
+  {
+    prompt: "let, const, and var can all be used to define variables",
+    answer: "correct",
+  },
+  {
+    prompt: "there will be no errors if you redefine a constant",
+    answer: "incorrect",
+    /* a const variable can't be redefined */
+  },
+  {
+    prompt: "There are six data types in JS",
+    answer: "incorrect",
+    /* there are five: string, number, object, undefined, Boolean */
+  },
+  {
+    prompt: "three equal signs is the strict equality operator",
+    answer: "correct",
+  },
+  {
+    prompt: "backslash in the escape character in JS",
+    answer: "correct",
+  },
+];
