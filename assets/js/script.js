04639f1a2791249753f45b393416eb380eafe18e
HankHint <hankhint@gmail.com>
2021-10-16 21:46:58 -0400

    start button disappears when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,82 @@
-var questions = [
-    {
-    prompt: "Javascript first appeared in 1995",
-    answer: "True"
-}
-{
-    prompt: "Event bubbling and event capturing are the same",
-    answer: "False"
 
-    /*With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
-
-With capturing, the event is first captured by the outermost element and propagated to the inner elements.*/
-}
-{
-    prompt: "Event delegation uses event bubbling",
-    answer: "True"
-}
-{
-    prompt: "the \<link\> tag is used to link the HTML to the javascript file",
-    answer: "False"
-    /* <script> tag is used */
-}
-{
-    prompt: "let, const, and var can all be used to define variables",
-    answer: "True"
-}
-{
-    prompt: "there will be no errors if you redefine a constant",
-    answer: "False"
-    /* a const variable can't be redefined */
-}
-{
-    prompt: "There are six data types in JS",
-    answer: "False"
-    /* there are five: string, number, object, undefined, Boolean */
-}
-{
-    prompt: "three equal signs is the strict equality operator",
-    answer: "True"
-}
-{
-    prompt: "backslash in the escape character in JS",
-    answer: "True"
-}
-]
 
 var score = 0;
 
-for (var i=0; i < questions.length; i++ ) {
+const startBtn = document.getElementById("startBtn");
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('prompt');
+const btnGridElement = document.getElementById('btnGrid')
+
+// these two variables need to be redefined later, so use let: 
+let shuffledQuestions, currentQuestionIndex;
+
+startBtn.addEventListener('click', startGame);
+
+function startGame() {
+    console.log("here")
+  startBtn.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+function showQuestion(question) {
+questionElement.innerText = question.prompt;
+
+}
+function selectAnswer() {}
+
+function resetState {
     
 }
 
-FormData.addEventListener('submit', e => {
-    e.preventDefault()
-})
+const questions = [
+    {
+      prompt: "Javascript first appeared in 1995",
+      answer: "True",
+    },
+    {
+      prompt: "Event bubbling and event capturing are the same",
+      answer: "False",
+  
+      /*With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
+  
+       With capturing, the event is first captured by the outermost element and propagated to the inner elements.*/
+    },
+    {
+      prompt: "Event delegation uses event bubbling",
+      answer: "True",
+    },
+    {
+      prompt: "the <link> tag is used to link the HTML to the javascript file",
+      answer: "False",
+      /* <script> tag is used */
+    },
+    {
+      prompt: "let, const, and var can all be used to define variables",
+      answer: "True",
+    },
+    {
+      prompt: "there will be no errors if you redefine a constant",
+      answer: "False",
+      /* a const variable can't be redefined */
+    },
+    {
+      prompt: "There are six data types in JS",
+      answer: "False",
+      /* there are five: string, number, object, undefined, Boolean */
+    },
+    {
+      prompt: "three equal signs is the strict equality operator",
+      answer: "True",
+    },
+    {
+      prompt: "backslash in the escape character in JS",
+      answer: "True",
+    },
+  ];
