989aef570e50968a0b279644197310731642b878
HankHint <hankhint@gmail.com>
2021-10-17 20:15:52 -0400

    added countdown timer and test question appears

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,4 @@
-var score = 0;
-
-const startBtn = document.getElementById("startBtn");
-const questionContainerElement = document.getElementById("question-container");
-const questionElement = document.getElementById("prompt");
-const btnGridElement = document.getElementById("btnGrid");
-
-// these two variables need to be redefined later, so use let:
-let shuffledQuestions, currentQuestionIndex;
-
-//timer
-
-var timerEl = document.getElementById('timer');
-var mainEl = document.getElementById('timer-container');
-
-function countdown() {
-    var timeLeft = 85;
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = 'Time is up.';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-      }
-    }, 1000);
-  }
-  
-
-startBtn.addEventListener("click", () => {
-  countdown();
-  startGame();
-});
-
-function startGame() {
-  startBtn.classList.add("hide");
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-  questionContainerElement.classList.remove("hide");
-  setNextQuestion();
-}
-
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.prompt;
-  
-}
-function selectAnswer() {}
-
-
-
-
-
-
+// Quiz Questions
 const questions = [
   {
     prompt: "Javascript first appeared in 1995",
@@ -78,8 +9,8 @@ const questions = [
     answer: "incorrect",
 
     /*With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
-  
-       With capturing, the event is first captured by the outermost element and propagated to the inner elements.*/
+    
+         With capturing, the event is first captured by the outermost element and propagated to the inner elements.*/
   },
   {
     prompt: "Event delegation uses event bubbling",
@@ -113,3 +44,81 @@ const questions = [
     answer: "correct",
   },
 ];
+
+//seting global variables
+const startBtnElement = document.getElementById("startBtn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const btnGridElement = document.getElementById("btnGrid");
+const correctEl = document.querySelector("#trueBtn");
+const incorrectEl = document.querySelector("#falseBtn");
+
+// timer
+let answer;
+var score = 0;
+var timeLeft;
+
+//timer
+
+var timerEl = document.getElementById("timer");
+
+function countdown() {
+  var timeLeft = 85;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + " seconds remaining";
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + " second remaining";
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      console.log(timeLeft);
+      timerEl.textContent = "Time is up.";
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+// Listens for the start button to be clicked, starts countdown and game
+
+startBtn.addEventListener("click", () => {
+  countdown();
+ startGame();
+});
+
+// Attach event listener to TRUE button element
+correctEl.addEventListener("click", function () {
+  answer = "correct";
+});
+
+// Attach event listener to FALSE button element
+incorrectEl.addEventListener("click", function () {
+  answer = "incorrect";
+});
+
+// Starts the game
+function startGame() {
+  //hides start button after it is clicked
+  startBtn.classList.add("hide");
+
+   //shows question and true false buttons after start is clicked
+  questionContainerElement.classList.remove("hide");
+
+ /* // loop
+  for (let gameState = 0; timeLeft > 0; gameState++) {
+    //push question
+    
+    //listen for answer
+
+    //check answer
+
+    //push new question */
+}
